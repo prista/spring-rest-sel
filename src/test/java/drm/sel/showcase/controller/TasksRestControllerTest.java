2290fa9cadcc2a909ab991317b63cf34ffd34dc5
prista <games.prista@mail.ru>
2026-03-09 19:02:55 +0300

    #unit tests, part 2

--- src/test/java/drm/sel/showcase/controller/TasksRestControllerTest.java
+++ src/test/java/drm/sel/showcase/controller/TasksRestControllerTest.java
@@ -1,7 +1,9 @@
 package drm.sel.showcase.controller;
 
+import drm.sel.showcase.model.NewTaskPayload;
 import drm.sel.showcase.model.Task;
 import drm.sel.showcase.repository.TaskRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,13 +12,15 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
+import java.util.Locale;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.doReturn;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TasksRestControllerTest {
@@ -29,6 +33,7 @@ class TasksRestControllerTest {
     TasksRestController underTest;
 
     @Test
+    @DisplayName("GET /api/tasks - returns HTTP-response with status 200 and body with all tasks")
     void handleGetAllTasks_ReturnsValidResponseEntity() {
         // given
         var tasks = List.of(
@@ -46,5 +51,49 @@ class TasksRestControllerTest {
         assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
         assertEquals(tasks, responseEntity.getBody());
     }
-  
+
+    @Test
+    void handleCreateTask_PayloadIsValid_ReturnsValidResponseEntity() {
+        // given
+        var details = "Третья задача";
+
+        // when
+        var responseEntity = underTest.handleCreateTask(
+                new NewTaskPayload(details),
+                // no matter what the value is
+                UriComponentsBuilder.fromUriString("http://localhost:8080"),
+                Locale.ENGLISH);
+
+        // then
+        assertNotNull(responseEntity);
+        assertEquals(HttpStatus.CREATED, responseEntity.getStatusCode());
+        assertEquals(MediaType.APPLICATION_JSON, responseEntity.getHeaders().getContentType());
+        if (responseEntity.getBody() instanceof Task task) {
+            assertNotNull(task.id());
+            assertEquals(details, task.details());
+            assertFalse(task.completed());
+
+            assertEquals(URI.create("http://localhost:8080/api/tasks/" + task.id()),
+                    responseEntity.getHeaders().getLocation());
+
+            verify(this.taskRepository).save(task);
+        } else {
+            assertInstanceOf(Task.class, responseEntity.getBody());
+        }
+        verifyNoMoreInteractions(this.taskRepository);
+    }
+
+    @Test
+    void handleCreateTask_PayloadIsInvalid_ReturnsValidResponseEntity() {
+        // given
+        var details = "   "; // blank
+        var local = Locale.US;
+        var errorMessage = "Details must be set";
+
+        doReturn(errorMessage).when(this.messageSource).getMessage(
+                "tasks.create.details.errors.not_set",
+                new Object[0],
+                local);
+
+    }
 }
